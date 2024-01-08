80113fae42a90381ecc2a597d3e5c20794d065fd
purankhoeval <89880917+purankhoeval@users.noreply.github.com>
2024-01-08 17:38:12 -0500

    Update app.py

--- zephyr-on-ai-core/serving/app.py
+++ zephyr-on-ai-core/serving/app.py
@@ -1,1 +1,26 @@
- 
+from fastapi import FastAPI, Request
+from model_pipeline import Model
+
+api = FastAPI()
+
+@api.on_event("startup")
+async def on_app_start():
+    """this function is called on startup and facilitates the loading and setup of the model for inference"""
+    Model.setup()
+
+@api.post("/v2/query")
+async def predict(request: Request):
+    """this function exposes the inference endpoint, expecting a json object with the prompt and a dictionary of arguments for the model"""
+    request_content = await request.json()
+    return Model.query(request_content["prompt"])
+
+@api.post("/v2/DataIngestion")
+async def ingest():
+    """this function exposes the inference endpoint, expecting a json object with the prompt and a dictionary of arguments for the model"""
+    # request_content = await request.json()
+    return Model.DataIngestion()
+
+if __name__ == "__main__":
+    # local testing
+    import uvicorn
+    uvicorn.run("app:api", host="0.0.0.0", port=8080, log_level="debug") 
